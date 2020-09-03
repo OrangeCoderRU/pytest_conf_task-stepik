90cb3f91e52a94c40902304b8d608dba545a5169
OrangeCoderRU <max.sablin@mail.ru>
2020-09-03 16:47:50 +0300

    create find element func

--- test_items.py
+++ test_items.py
@@ -1,9 +1,17 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
+def element_is_exist(browser, selector):
+    try:
+        browser.find_element(By.CSS_SELECTOR, selector)
+        return True
+    except:
+        return False
+
 def test_items_link(browser):
     browser.get(link)
-    time.sleep(10)
+    assert element_is_exist(browser, "#add_to_basket_form"),  "Кнопки добавления в корзину нет"
