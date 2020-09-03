18882e1b0ae1a5ed349f41a8954c16b8b969f523
OrangeCoderRU <max.sablin@mail.ru>
2020-09-03 15:13:46 +0300

    add and open link

--- test_items.py
+++ test_items.py
@@ -1,2 +1,8 @@
 import pytest
 from selenium import webdriver
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_items_link(browser):
+    browser.get(link)
+    
