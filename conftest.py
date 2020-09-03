afe7a0b95fda5b0521cbaa9cd9bcc849574f97f5
OrangeCoderRU <max.sablin@mail.ru>
2020-09-03 15:19:23 +0300

    upd conf

--- conftest.py
+++ conftest.py
@@ -15,8 +15,6 @@ def browser(request):
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
         browser = webdriver.Firefox()
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
     browser.quit()
